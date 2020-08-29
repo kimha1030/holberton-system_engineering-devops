c8c120c5911b485f17c5be9d6bd7cf2547019b00
kimha1030 <1478@holbertonschool.com>
2020-08-29 00:00:31 -0500

    Update task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Automate the task of create a custom HTTP header response
 exec { 'http':
     command  => 'sudo apt-get -y update && sudo apt-get -y install nginx &&
-             sudo sed -i "27iadd_header X-Served
+             sudo sed -i "27i \add_header X-Served
 -By:
-             $hostname always;" /etc/nginx/sites-available/default &&
+             \$hostname always;" /etc/nginx/sites-available/default &&
              sudo service nginx start',
     provider => shell,
 }
