cb87711170b66ee7a5176fcdf8bee619a63d7fe7
kimha1030 <1478@holbertonschool.com>
2020-09-17 12:57:02 -0500

    Add task 0 and readme file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+"""Task 0. How many subs?"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """This function allow generate the count of suscribers"""
+    userAgent = {'User-agent': 'Sure_Student_8276'}
+    urlReddit = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    req = requests.get(urlReddit, headers=userAgent)
+    reqJson = req.json()
+    if req.status_code == 404:
+        return (0)
+    else:
+        return(reqJson["data"]["subscribers"])
