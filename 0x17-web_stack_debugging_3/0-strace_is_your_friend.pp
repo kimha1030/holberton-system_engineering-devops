444e911f0803907cec85ab5fa4bb1635a1872eed
kimha1030 <1478@holbertonschool.com>
2020-09-30 11:48:38 -0500

    Update file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Task1: Execute a command to fix a site wordpress
-exec { 'fix-wordpress':
-    command => 'sed -i 's/phpp/php/' /var/www/html/wp-settings.php',
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+exec {'fix wordpress.':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
