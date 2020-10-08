402b4e066fb5d0e53f0695c638ac7e3c930cf50e
kimha1030 <1478@holbertonschool.com>
2020-10-08 17:28:03 -0500

    Add task advanced

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Login with holberton user and open a file without any error message.
+exec { 'change-max-open-files-hard-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/hard nofile [0-9]\+/hard nofile 4000/g'"
+}
+exec { 'change-max-open-files-soft-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/soft nofile [0-9]\+/soft nofile 3000/g'"
+}
